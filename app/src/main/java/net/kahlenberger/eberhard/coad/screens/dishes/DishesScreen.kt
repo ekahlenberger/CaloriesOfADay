63e03fa5b744f96913543d44f04ab3ef658bbf57
Eberhard Kahlenberger <github@e.kahlenberger.net>
2023-05-29 11:34:03 +0200

    reformated dish display

--- app/src/main/java/net/kahlenberger/eberhard/coad/screens/dishes/DishesScreen.kt
+++ app/src/main/java/net/kahlenberger/eberhard/coad/screens/dishes/DishesScreen.kt
@@ -10,20 +10,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import net.kahlenberger.eberhard.coad.screens.Screen
@@ -37,6 +40,7 @@ fun DishesScreen(viewModel: DishesViewModel, innerPadding: PaddingValues, navCon
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
+            .padding(8.dp)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -45,6 +49,7 @@ fun DishesScreen(viewModel: DishesViewModel, innerPadding: PaddingValues, navCon
             Text(
                 text = "Dishes",
                 style = MaterialTheme.typography.h5,
+                //color = MaterialTheme.colors.primary,
                 modifier = Modifier.padding(8.dp),
             )
 
@@ -52,7 +57,7 @@ fun DishesScreen(viewModel: DishesViewModel, innerPadding: PaddingValues, navCon
             {
                 LazyColumn {
                     items(dishes) { dish ->
-                        DishItem(dish = dish, viewModel = viewModel)
+                        DishItem(dish = dish, onDelete = {d ->  viewModel.deleteDish(d) })
                         Spacer(modifier = Modifier.height(8.dp))
                     }
                 }
@@ -74,27 +79,43 @@ fun DishesScreen(viewModel: DishesViewModel, innerPadding: PaddingValues, navCon
 }
 
 @Composable
-fun DishItem(dish: Dish, viewModel: DishesViewModel) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column {
-            Text(text = dish.name)
-            Text(text = "Direct children: ${dish.childDishes.size}")
-            Text(text = "Total calories: ${dish.totalCalories}")
+fun DishItem(dish: Dish, onDelete: (Dish) -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = dish.name,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = "${dish.totalCalories} kcal",
+                style = MaterialTheme.typography.body1,
+                textAlign = TextAlign.End
+            )
+            IconButton(onClick = { onDelete(dish) }) {
+                Icon(Icons.Filled.Delete, contentDescription = "Delete Consumed Item")
+            }
         }
+        if (dish.childDishes.size > 1)
+        {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = dish.childDishes.sortedBy { it.totalCalories }.joinToString(separator = ", ") { it.name },
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.weight(1f)
+                )
+            }
 
-        Column {
-            Button(onClick = { /* Edit action */ }) {
-                Text(text = "Edit")
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(onClick = { viewModel.deleteDish(dish) }) {
-                Text(text = "Delete")
-            }
         }
     }
 }
